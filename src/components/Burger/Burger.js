426e06515f00632d3fe6aca89eed8aabbc0839fa
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2019-01-30 18:32:17 -0500

    Ingredients output dynamically Setup state inside burger builder transformed state into an array for burger component to pass props and receive keyvalues of new array mapping is fun.........

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,26 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)//object is default js, has a keys method that extracts the keys of an object and turns into an array, not the values
+        .map(igKey => { 
+        /*exectutes a function on each element on input array, "igKey" is random name to describe ingredient props from state
+        transform string value into array into as many elements as we have ingredients for the given ingredient 
+        spread operator spreads new array that needs to be contructed, 
+        constructed with Array() method from js, Arrray() inside parenthesis array determined by props,
+        key is accessed with igKey as we are still in the map function that contains all the keys,
+        execute map on that array, underscore for argument name, index is what we care about
+        return burgerigredient in that method where we set the key to the ingredient key "igkey"(salad, meat) and the ingredient value to "i"(1,2) 
+        keys we use must match ingredients*/    
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+           return <BurgerIngredient key={igKey + i} type={igKey} />;
+        })
+        ;
+        });
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/> {/* child component, type refers to each case created*/}
-            <BurgerIngredient type="cheese"/> {/* must be a string as thats what we determined via prototype*/}
-            <BurgerIngredient type="meat"/>
-            <BurgerIngredient type="bread-bottom"/>
+            {transformedIngredients} {/*ingredients will always change where as burgertop and bottom is needed*/}
+            <BurgerIngredient type="bread-bottom"/>  {/* must be a string as thats what we determined via prototype*/}
         </div> /*deliberately a div to give this a wrapper styling */
     );
 };
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,25 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    // constructor(props) { /*another approach on how to set up our state*/
+    //     super(props);
+    //     this.state = {...}
+    // }
+    
+    state = {
+        ingredients: { //key value pairs for ingredients object, keys are name of ingreident, value is amount
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+
+        }
+    }
+
     render(){
         return(
             <Aux>
-                <Burger /> {/*graphic representation of burger that was built*/}
+                <Burger ingredients={this.state.ingredients}/> {/*graphic representation of burger that was built*/}
                 <div>Build Controls</div> {/*area to add and remove ingredients*/}
             </Aux>
         );
