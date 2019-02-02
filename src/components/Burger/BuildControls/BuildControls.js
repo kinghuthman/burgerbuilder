0b24bdf3769e45a0292a562f58c1f5b6f0b1730e
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2019-02-02 12:21:49 -0500

    addIngredientHandler functional, has access to state and is immutable updates price(not seen) and ingredient image(displayed) buildControls has a reference to the handler buildControl receives this handler as props set to onClick

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -16,11 +16,17 @@ call map on controls to access array
 and map each 'ctrl' into a self closing <BuildControl />
 set key to the label as its a unique identifier
 also pass label(like props) to the buildcontrol component label
+receieved added props from parent BurgerBuilder
+
+need to be aware of what type the buildcontrol has, use a es6 to callback ctrl.type through handler
 */
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
         {controls.map(ctrl => (
-            <BuildControl key={ctrl.label} label={ctrl.label} />
+            <BuildControl 
+            key={ctrl.label} 
+            label={ctrl.label} 
+            added={() => props.ingredientAdded(ctrl.type)}/> 
         ))}
     </div>
     
