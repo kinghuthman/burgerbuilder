7b9e69781eb18901e470bfa12a18c787b3af4400
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2019-02-02 18:28:53 -0500

    Toolbar & Logo Toolbar added to layout.js so its displayed on each page as that is our wrapper<Aux/>

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = ( props ) => (
     <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div> {/* replace with three components*/}
+        <Toolbar/>
                    <main className={classes.Content}> 
                 {props.children} 
             </main>
